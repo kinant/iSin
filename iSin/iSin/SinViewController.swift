56b4c3c9763ed68c2f2ad14546a3639fb1a8f943
Kinan Turman <kinant@Kinans-MacBook-Pro.local>
2016-03-21 15:52:26 -0600

    added swiped tab bar. uses only one view controller

---
+++ iSin/iSin/SinViewController.swift
@@ -0,0 +1,26 @@
+//
+//  SinViewController.swift
+//  iSin
+//
+//  Created by Kinan Turman on 3/21/16.
+//  Copyright © 2016 Kinan Turman. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SinViewController: UIViewController {
+
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var addButton: UIButton!
+    var bckColor: UIColor!
+    var sinTitle: String!
+    
+    override func viewWillAppear(animated: Bool) {
+        self.view.backgroundColor = bckColor;
+        self.titleLabel.text = sinTitle;
+    }
+    
+
+}
--- iSin/iSin/ViewController.swift
+++ iSin/iSin/ViewController.swift
@@ -8,18 +8,79 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: PagerController, PagerDataSource {
+    
+    var titles: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.dataSource = self
+        
+        // Instantiating Storyboard ViewControllers
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let controller1 = storyboard.instantiateViewControllerWithIdentifier("SinView") as! SinViewController
+        controller1.sinTitle = "LUST"
+        controller1.bckColor = UIColor.blueColor();
+        
+        let controller2 = storyboard.instantiateViewControllerWithIdentifier("SinView") as! SinViewController
+        controller2.sinTitle = "GLUTTONY"
+        controller2.bckColor = UIColor.brownColor()
+        
+        let controller3 = storyboard.instantiateViewControllerWithIdentifier("SinView") as! SinViewController
+        controller3.sinTitle = "GREED"
+        controller3.bckColor = UIColor.orangeColor()
+        
+        let controller4 = storyboard.instantiateViewControllerWithIdentifier("SinView") as! SinViewController
+        controller4.sinTitle = "SLOTH"
+        controller4.bckColor = UIColor.yellowColor()
+        
+        let controller5 = storyboard.instantiateViewControllerWithIdentifier("SinView") as! SinViewController
+        controller5.sinTitle = "WRATH"
+        controller5.bckColor = UIColor.whiteColor()
+        
+        let controller6 = storyboard.instantiateViewControllerWithIdentifier("SinView") as! SinViewController
+        controller6.sinTitle = "ENVY"
+        controller6.bckColor = UIColor.redColor()
+        
+        let controller7 = storyboard.instantiateViewControllerWithIdentifier("SinView") as! SinViewController
+        controller7.sinTitle = "PRIDE"
+        controller7.bckColor = UIColor.greenColor()
+        
+        // Setting up the PagerController with Name of the Tabs and their respective ViewControllers
+        self.setupPager(
+            tabNames: ["lust", "gluttony", "greed", "sloth", "wrath", "envy", "pride"],
+            tabControllers: [controller1, controller2, controller3, controller4, controller5, controller6, controller7])
+        
+        customiseTab()
     }
-
+    
+    // Customising the Tab's View
+    func customiseTab()
+    {
+        indicatorColor = UIColor.whiteColor()
+        tabsViewBackgroundColor = UIColor(rgb: 0x00AA00)
+        contentViewBackgroundColor = UIColor.grayColor().colorWithAlphaComponent(0.32)
+        
+        startFromSecondTab = false
+        centerCurrentTab = false
+        tabLocation = PagerTabLocation.Top
+        tabHeight = 49
+        tabOffset = 36
+        tabWidth = 96.0
+        fixFormerTabsPositions = false
+        fixLaterTabsPosition = false
+        animation = PagerAnimation.During
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // Programatically selecting a tab. This function is getting called on AppDelegate
+    func changeTab() {
+        //self.selectTabAtIndex(4)
+    }
 }
+
 
